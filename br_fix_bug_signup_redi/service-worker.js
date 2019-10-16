62ec75e5d281c3470167e0ff838ad7cb34efa294
ali.afsah@regentmarkets.com <ali.afsah@regentmarkets.com>
2019-10-16 13:33:29 +0330

    Updates

---
+++ br_fix_bug_signup_redi/service-worker.js
@@ -0,0 +1,33 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/br_fix_bug_signup_redi/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/br_fix_bug_signup_redi/workbox-v4.3.1"});
+
+importScripts(
+  "/br_fix_bug_signup_redi/precache-manifest.d1ffd822cf4529dbfb76baede0e82689.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.precaching.cleanupOutdatedCaches();
